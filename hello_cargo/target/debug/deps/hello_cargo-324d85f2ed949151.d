dbf5bfdacee6dda48b8038f47a97c303934de918
giggle-giraffe <giggle.giraffe.bj@gmail.com>
2024-10-12 01:12:15 +0800

    hello_cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-324d85f2ed949151.d
@@ -0,0 +1,5 @@
+C:\Users\tl_28\Documents\giggle_giraffe\02_learn_rust\learn_rust\hello_cargo\target\debug\deps\libhello_cargo-324d85f2ed949151.rmeta: src/main.rs
+
+C:\Users\tl_28\Documents\giggle_giraffe\02_learn_rust\learn_rust\hello_cargo\target\debug\deps\hello_cargo-324d85f2ed949151.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\Users\tl_28\Documents\giggle_giraffe\02_learn_rust\learn_rust\hello_cargo\target\debug\deps\hello_cargo.exe: src/main.rs
+
+C:\Users\tl_28\Documents\giggle_giraffe\02_learn_rust\learn_rust\hello_cargo\target\debug\deps\hello_cargo.d: src/main.rs
+
+src/main.rs:
