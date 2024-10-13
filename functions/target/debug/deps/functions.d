62b7fd98aa7b338e1fee4f834452e4af02200e65
giggle-giraffe <giggle.giraffe.bj@gmail.com>
2024-10-14 01:09:08 +0800

    functions

---
+++ functions/target/debug/deps/functions.d
@@ -0,0 +1,5 @@
+C:\Users\tl_28\Documents\giggle_giraffe\02_learn_rust\learn_rust\functions\target\debug\deps\functions.exe: src/main.rs
+
+C:\Users\tl_28\Documents\giggle_giraffe\02_learn_rust\learn_rust\functions\target\debug\deps\functions.d: src/main.rs
+
+src/main.rs:
---
+++ functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+C:\Users\tl_28\Documents\giggle_giraffe\02_learn_rust\learn_rust\functions\target\debug\functions.exe: C:\Users\tl_28\Documents\giggle_giraffe\02_learn_rust\learn_rust\functions\src\main.rs
