04c420df4510c8485f6bc3a599a1dafa73c032aa
giggle-giraffe <giggle.giraffe.bj@gmail.com>
2024-10-14 01:24:53 +0800

    branches

---
+++ branches/target/debug/branches.d
@@ -0,0 +1,1 @@
+C:\Users\tl_28\Documents\giggle_giraffe\02_learn_rust\learn_rust\branches\target\debug\branches.exe: C:\Users\tl_28\Documents\giggle_giraffe\02_learn_rust\learn_rust\branches\src\main.rs
---
+++ branches/target/debug/deps/branches.d
@@ -0,0 +1,5 @@
+C:\Users\tl_28\Documents\giggle_giraffe\02_learn_rust\learn_rust\branches\target\debug\deps\branches.exe: src/main.rs
+
+C:\Users\tl_28\Documents\giggle_giraffe\02_learn_rust\learn_rust\branches\target\debug\deps\branches.d: src/main.rs
+
+src/main.rs:
