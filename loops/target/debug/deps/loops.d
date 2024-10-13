6e1f13f5dbc73b92610e4914fa599472bbe6b745
giggle-giraffe <giggle.giraffe.bj@gmail.com>
2024-10-14 01:35:24 +0800

    loops

---
+++ loops/target/debug/deps/loops.d
@@ -0,0 +1,5 @@
+C:\Users\tl_28\Documents\giggle_giraffe\02_learn_rust\learn_rust\loops\target\debug\deps\loops.exe: src/main.rs
+
+C:\Users\tl_28\Documents\giggle_giraffe\02_learn_rust\learn_rust\loops\target\debug\deps\loops.d: src/main.rs
+
+src/main.rs:
---
+++ loops/target/debug/loops.d
@@ -0,0 +1,1 @@
+C:\Users\tl_28\Documents\giggle_giraffe\02_learn_rust\learn_rust\loops\target\debug\loops.exe: C:\Users\tl_28\Documents\giggle_giraffe\02_learn_rust\learn_rust\loops\src\main.rs
