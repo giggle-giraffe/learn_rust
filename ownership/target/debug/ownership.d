a1b6bb787430046a80ae2ea3fb4ac0edaf9b64cb
giggle-giraffe <giggle.giraffe.bj@gmail.com>
2024-10-15 01:21:35 +0800

    ownership

---
+++ ownership/target/debug/ownership.d
@@ -0,0 +1,1 @@
+C:\Users\tl_28\Documents\giggle_giraffe\02_learn_rust\learn_rust\ownership\target\debug\ownership.exe: C:\Users\tl_28\Documents\giggle_giraffe\02_learn_rust\learn_rust\ownership\src\main.rs
