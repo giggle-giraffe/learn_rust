fe1a417d6590c806072c148c249c23ba87145918
giggle-giraffe <giggle.giraffe.bj@gmail.com>
2024-10-16 01:52:59 +0800

    structs

---
+++ structs/target/debug/deps/structs.d
@@ -0,0 +1,5 @@
+C:\Users\tl_28\Documents\giggle_giraffe\02_learn_rust\learn_rust\structs\target\debug\deps\structs.exe: src/main.rs
+
+C:\Users\tl_28\Documents\giggle_giraffe\02_learn_rust\learn_rust\structs\target\debug\deps\structs.d: src/main.rs
+
+src/main.rs:
---
+++ structs/target/debug/structs.d
@@ -0,0 +1,1 @@
+C:\Users\tl_28\Documents\giggle_giraffe\02_learn_rust\learn_rust\structs\target\debug\structs.exe: C:\Users\tl_28\Documents\giggle_giraffe\02_learn_rust\learn_rust\structs\src\main.rs
