51874e5f9ef2f5bf36ac00687f90eebe4cadb083
giggle-giraffe <giggle.giraffe.bj@gmail.com>
2024-10-16 02:16:52 +0800

    rectangles

---
+++ rectangles/target/debug/deps/rectangles.d
@@ -0,0 +1,5 @@
+C:\Users\tl_28\Documents\giggle_giraffe\02_learn_rust\learn_rust\rectangles\target\debug\deps\rectangles.exe: src/main.rs
+
+C:\Users\tl_28\Documents\giggle_giraffe\02_learn_rust\learn_rust\rectangles\target\debug\deps\rectangles.d: src/main.rs
+
+src/main.rs:
---
+++ rectangles/target/debug/rectangles.d
@@ -0,0 +1,1 @@
+C:\Users\tl_28\Documents\giggle_giraffe\02_learn_rust\learn_rust\rectangles\target\debug\rectangles.exe: C:\Users\tl_28\Documents\giggle_giraffe\02_learn_rust\learn_rust\rectangles\src\main.rs
