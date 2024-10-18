173c9b2326f6ddef2e143b277785a61f44be4244
giggle-giraffe <giggle.giraffe.bj@gmail.com>
2024-10-19 00:29:10 +0800

    crates

---
+++ restaurant/target/debug/deps/restaurant-320fcb3f5fcddba7.d
@@ -0,0 +1,9 @@
+C:\Users\tl_28\Documents\giggle_giraffe\02_learn_rust\learn_rust\restaurant\target\debug\deps\librestaurant-320fcb3f5fcddba7.rmeta: src\lib.rs src\front_of_house.rs src\front_of_house\hosting.rs
+
+C:\Users\tl_28\Documents\giggle_giraffe\02_learn_rust\learn_rust\restaurant\target\debug\deps\librestaurant-320fcb3f5fcddba7.rlib: src\lib.rs src\front_of_house.rs src\front_of_house\hosting.rs
+
+C:\Users\tl_28\Documents\giggle_giraffe\02_learn_rust\learn_rust\restaurant\target\debug\deps\restaurant-320fcb3f5fcddba7.d: src\lib.rs src\front_of_house.rs src\front_of_house\hosting.rs
+
+src\lib.rs:
+src\front_of_house.rs:
+src\front_of_house\hosting.rs:
