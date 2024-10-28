2cc206921ae42b8d401ec877627956ff8b4136fb
giggle-giraffe <giggle.giraffe.bj@gmail.com>
2024-10-29 01:23:28 +0800

    common collections

---
+++ collections/target/debug/collections.d
@@ -0,0 +1,1 @@
+C:\Users\tl_28\Documents\giggle_giraffe\02_learn_rust\learn_rust\collections\target\debug\collections.exe: C:\Users\tl_28\Documents\giggle_giraffe\02_learn_rust\learn_rust\collections\src\main.rs
