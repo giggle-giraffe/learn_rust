6a6fed6ea2a4a674bb4026f4874b2db04cd460a5
giggle-giraffe <giggle.giraffe.bj@gmail.com>
2024-11-02 21:59:48 +0800

    minigrep_1

---
+++ minigrep/target/debug/deps/minigrep-861e22b6abefb451.d
@@ -0,0 +1,5 @@
+C:\Users\tl_28\Documents\giggle_giraffe\02_learn_rust\learn_rust\minigrep\target\debug\deps\libminigrep-861e22b6abefb451.rmeta: src\lib.rs
+
+C:\Users\tl_28\Documents\giggle_giraffe\02_learn_rust\learn_rust\minigrep\target\debug\deps\minigrep-861e22b6abefb451.d: src\lib.rs
+
+src\lib.rs:
---
+++ minigrep/target/debug/deps/minigrep-c0303a340235aef7.d
@@ -0,0 +1,5 @@
+C:\Users\tl_28\Documents\giggle_giraffe\02_learn_rust\learn_rust\minigrep\target\debug\deps\libminigrep-c0303a340235aef7.rmeta: src/main.rs
+
+C:\Users\tl_28\Documents\giggle_giraffe\02_learn_rust\learn_rust\minigrep\target\debug\deps\minigrep-c0303a340235aef7.d: src/main.rs
+
+src/main.rs:
