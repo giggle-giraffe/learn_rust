3d4de8a085624dda4ad5db6d9d50f5557081cc95
giggle-giraffe <giggle.giraffe.bj@gmail.com>
2025-01-20 17:18:45 +0800

    9 methods

---
+++ rust_by_practice/09_methods/q6/src/main.rs
@@ -0,0 +1,25 @@
+#[derive(Debug)]
+enum TrafficLightColor {
+    Red,
+    Yellow,
+    Green,
+}
+
+// Implement TrafficLightColor with a method.
+impl TrafficLightColor {
+    fn color(&self) -> &str {
+        match self {
+            TrafficLightColor::Red => "red",
+            TrafficLightColor::Yellow => "yellow",
+            TrafficLightColor::Green => "green",
+        }
+    }
+}
+
+fn main() {
+    let c = TrafficLightColor::Yellow;
+
+    assert_eq!(c.color(), "yellow");
+
+    println!("{:?}",c);
+}
